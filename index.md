bf05f419fdd452ca7aab925e7e8e52f3e5abf921
Md Rakibul Hasan <rakibul.eeekuet@gmail.com>
2022-03-12 23:57:07 +0600

    MAJOR UPDATE: Jekyll to Pelican

--- index.md
+++
@@ -1,9 +0,0 @@
----
-layout: default
----
-
-Hi there!  
-This is **Md. Rakibul Hasan**
-
-I started this website mainly to track down my recent involvments in research, teaching, and so on. 
-
---
+++ pelicanconf.py
@@ -0,0 +1,38 @@
+AUTHOR = 'M Rakibul Hasan (Rakib)'
+SITENAME = 'M Rakibul Hasan'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Asia/Dhaka'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('BRAC University','https://www.bracu.ac.bd/about/people/md-rakibul-hasan'),
+        ('LinkedIn','https://www.linkedin.com/in/rakibul-eeekuet/'),
+        ('ORCiD','https://orcid.org/0000-0003-2565-5321'),
+        ('ReserchGate','https://www.researchgate.net/profile/Md-Rakibul-Hasan-13'),
+        ('GitHub','https://github.com/mrh-rakib'),
+        ('e-mail','mailto:rakibul.hasan@bracu.ac.bd'))
+
+
+
+# Social widget
+# SOCIAL = (('You can add links in your config file', '#'),
+        #   ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+# PAGE_URL = '{slug}'
+# PAGE_SAVE_AS = '{slug}'
